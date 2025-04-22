ba5da0954e319c3d532dcc6ede1d04eec90b0b42
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-22 17:35:06 -0300

    feat: conf app.modules with Prisma and User module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './users/module/user.module';
+import { UserModule } from './users/user.module';
 @Module({
   imports: [PrismaModule, UserModule],
 })
