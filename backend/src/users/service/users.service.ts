b5c4fee47e58f3bd764c3b5a32a0329bdf35d5de
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-22 17:32:09 -0300

    feat: service for create new user in database

---
+++ backend/src/users/service/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from "../dtos/user.dto";
+import * as bcrypt from "bcrypt";
+
+
+
+
+@Injectable()
+export class UserService { 
+    constructor(private prisma: PrismaService) {}
+
+    async create(data: CreateUserDto) {
+        const hashedPassword = await bcrypt.hash(data.password, 10)
+        const user = await this.prisma.user.create({
+            data: {
+                email: data.email,
+                name: data.name,
+                password: hashedPassword,
+            },
+        });
+
+    const { password, ...result} = user;
+    return result;
+    }
+}
