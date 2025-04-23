afe6256356b754e076a01ffd50c8576b0a62b693
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-23 18:25:02 -0300

    feat: auth controler with login endpoint

---
+++ backend/src/auth/controllers/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "src/auth/services/auth.service";
+import { LoginDto } from "src/auth/dtos/login.dto";
+
+
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() loginDto: LoginDto) {
+       const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+       return this.authService.login(user);
+    }
+}
