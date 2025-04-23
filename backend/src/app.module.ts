513e43ed58dd6f8e3db43faa4945d437b7e236df
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-23 18:31:02 -0300

    feat: import auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './database/prisma.module';
 import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, AuthModule],
 })
 export class AppModule {}
