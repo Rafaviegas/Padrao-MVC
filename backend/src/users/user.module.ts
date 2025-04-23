384c0bebc0bfd1f5d85d7ff7f4fffbe9c00ad5e0
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-23 18:23:17 -0300

    feat: conf user module

--- backend/src/users/user.module.ts
+++ backend/src/users/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { PrismaModule } from "src/database/prisma.module";
 import { UserController } from "src/users/controller/users.controller";
 import { UserService } from "src/users/service/users.service";
 
