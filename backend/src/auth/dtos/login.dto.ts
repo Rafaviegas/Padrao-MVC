562b4feb268aded890bea138d76ef183aa86d91b
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-24 13:38:32 -0300

    feat: Documented dtos with swagger

--- backend/src/auth/dtos/login.dto.ts
+++ backend/src/auth/dtos/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
-
 export class LoginDto {
+  
+  @ApiProperty({example: 'lucas@gmail.com'})
   @IsEmail()
   email: string;
 
+  @ApiProperty({example: "MeuGatinhoLindo123"})
   @IsNotEmpty()
   password: string;
 }
