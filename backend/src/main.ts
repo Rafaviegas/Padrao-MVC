769446ea46935e33926304b50e179b50d2efcc94
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-24 13:36:22 -0300

    feat: config swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Padrão MVC')
+    .setDescription('DOcumentação Swagger da API padrão MVC')
+    .setVersion('1.0')
+    .addTag('Padrão MVC')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
+
   app.useGlobalPipes(new ValidationPipe()); 
   await app.listen(port);
   console.log(`Application is running on: localhost:${port}`);
