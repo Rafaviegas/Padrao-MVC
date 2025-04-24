7c33cee48dbe95bbd0388ad7c9b926a917464701
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-24 17:38:20 -0300

    feat: import cloudwatch module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './database/prisma.module';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CloudwatchLoggerModule } from './cloudwatch-logger/cloudwatch-logger.module';
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [ PrismaModule, UserModule, AuthModule, CloudwatchLoggerModule],
 })
 export class AppModule {}
