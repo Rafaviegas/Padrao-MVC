c37fee47bef689bd53111ad5a9e8932452f2fd58
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-04-24 17:41:00 -0300

    feat: export logs for cloudwatch

--- backend/src/auth/services/auth.service.ts
+++ backend/src/auth/services/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { UserService } from "src/users/service/users.service";
 import { userEntity } from "src/auth/models/user.entity";
+import { CloudwatchLoggerService } from "src/cloudwatch-logger/cloudwatch-logger.service";
 import * as bcrypt from "bcrypt";
+import { access } from "fs";
 
 
 
@@ -10,7 +12,8 @@ import * as bcrypt from "bcrypt";
 export class AuthService {
     constructor (
         private readonly userService: UserService,
-        private readonly jwtService: JwtService
+        private readonly jwtService: JwtService,
+        private readonly logger: CloudwatchLoggerService
     ){}
 
     async validateUser(email: string, password: string): Promise<userEntity> {
@@ -18,13 +21,15 @@ export class AuthService {
         // Busca o usuario no banco de dados pelo email ultilizando o prisma
         const user = await this.userService.findByEmail(email);
         if (!user) {
+            this.logger.warn('Login falhou: email não encontrado' + { email });
             throw new UnauthorizedException('Usuario Não encontrado');
         }
 
 
-        // Se eu achar o usuario pelo email eu comparo as senhas ultilizando o bcrypt que ja gera o hash e compara com o banco
+        // Se eu achar o usuario pelo email, comparo as senhas ultilizando o bcrypt que ja gera o hash e compara com o banco
         const passwordIsValid = await bcrypt.compare(password, user.password)
         if (!passwordIsValid) {
+            this.logger.warn('Login falhou: Senha Invalida' + {email})
             throw new UnauthorizedException('Senha inválida');
         }
 
@@ -33,9 +38,11 @@ export class AuthService {
 
     async login(user: userEntity){
         const payload = {sub: user.id, email: user.email};
+        const token = this.jwtService.sign(payload)
+        this.logger.log("Login efetuado com sucesso: " + {userId: user.id, email: user.email});
         return {
-            access_token: this.jwtService.sign(payload),
-        };
+            access_token : token
+        }
     }
 
 
