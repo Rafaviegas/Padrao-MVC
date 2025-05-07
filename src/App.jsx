3e0002bd9815df7744a1bb9a8e777850e8c823a8
Rafaella Viegas <rviegas2004@gmail.com>
2025-05-06 22:48:53 -0300

    save work

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[email, setEmail] = useState('');
+  const[senha, setSenha] = useState('');
+  
+  function onSubmit(e) {
+    e.preventDefault()
+    console.log(email)
+  }
 
   return (
     <div id='login'>
-      <form>
+      <form onSubmit={onSubmit}>
         <h1>LOGIN</h1> 
-        <input className='mb35' type='text' placeholder='Email'/>
-        <input className='mb14' type='password' placeholder='Senha'/>
+        <input className='mb35' type='email' value={email} placeholder='Email' onChange={(e) => setEmail(e.target.value)} required/>
+        <input className='mb14' type='password' value={senha} placeholder='Senha' onChange={(e) => setSenha(e.target.value)} required/>
         <div className='create'>
           <p>Não possui uma conta?</p>
           <a href='#'>Criar</a>
         </div>
-        <button className='button'>Entrar</button>
+        <button type='submit' className='button'>Entrar</button>
       </form>
     </div>
   )
