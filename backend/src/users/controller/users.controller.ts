c1979d32c539840ddef88af32af019b77825be9d
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-06-23 08:11:53 -0300

    feat: controller crud user

--- backend/src/users/controller/users.controller.ts
+++ backend/src/users/controller/users.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, HttpCode, Param, Post, Put, Get, UseGuards} from "@nestjs/common";
 import { UserService } from "../service/users.service";
-import { CreateUserDto } from "../dtos/user.dto";
+import { CreateUserDto, EditUserDto } from "../dtos/user.dto";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guards";
+import { ApiBearerAuth } from "@nestjs/swagger";
+
 
 
 @Controller('users')
@@ -8,8 +11,43 @@ export class UserController {
     constructor(private userService: UserService) {}
 
     @Post()
+    @HttpCode(202)
     create(@Body() data: CreateUserDto) {
         return this.userService.create(data)
     };
 
+    @Delete(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth('jwt-auth')
+    @HttpCode(200)
+    delete(@Param('id') id: string) {
+        return this.userService.delete(id)
+    }
+
+    @Put(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth('jwt-auth')
+    @HttpCode(200)
+    edit(@Body() data: EditUserDto, @Param('id') id: string) {
+        return this.userService.edit(id, data)
+    }
+
+    @Get(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth('jwt-auth')
+    @HttpCode(200)
+    getById(@Param('id') id: string){
+        return this.userService.getById(id)
+    }
+
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth('jwt-auth')
+    @HttpCode(200)
+    getAll(){
+        return this.userService.getAll()
+    }
+    
+
 }
+
