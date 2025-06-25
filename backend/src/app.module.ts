b8c3873215aba84365fbb5061000dee06cf020ba
IgotOtonni <igorotoni2011@gmail.com>
2025-06-25 20:22:50 -0300

    subir app modules - leitor module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,8 +5,9 @@ import { AuthModule } from './auth/auth.module';
 import { CloudwatchLoggerModule } from './cloudwatch-logger/cloudwatch-logger.module';
 import { CategoriesModule } from './categories/categories.module';
 import { LivroModule } from './livro/livro.module';
+import { LeitorModule } from './leitor/reader.module';
 
 @Module({
-  imports: [ PrismaModule, UserModule, AuthModule, CloudwatchLoggerModule, CategoriesModule, LivroModule],
+  imports: [ PrismaModule, UserModule, AuthModule, CloudwatchLoggerModule, CategoriesModule, LivroModule, LeitorModule],
 })
 export class AppModule {}
